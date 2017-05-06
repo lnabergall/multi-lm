07967d2f7365aaef74066fba90178ad52d24bb17
Lukas Nabergall <lnabergall@mail.usf.edu>
2017-05-05 22:47:56 -0400

    Added simple example LSTM

---
+++ code/lstm_example_keras.py
@@ -0,0 +1,52 @@
+"""
+Example implementation of a vanilla LSTM network in Keras.
+"""
+
+import numpy as np
+import random
+import sys
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, LSTM
+from keras.optimizers import (RMSprop, SGD, Adagrad, 
+                              Adadelta, Adam, Adamax, Nadam)
+from keras.utils.data_utils import get_file
+
+import data_processing as dp
+
+
+HIDDEN_DIM = 128
+
+
+def build_model(train_data):
+    print("Building model...")
+    model = Sequential()
+    model.add(LSTM(HIDDEN_DIM, input_shape=(train_data.shape[1],)))
+    model.add(Dense(train_data.shape[1]))
+    model.add(Activation("softmax"))
+    optimizer = RMSprop(lr=0.01)
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+
+
+def sample(probability_array, temperature=1.0):
+    predications = np.asarray(probability_array).astype("float64")
+    predications = np.log(predications) / temperature
+    exp_predictions = np.exp(predications)
+    predications = exp_predictions / np.sum(exp_predictions)
+    return np.random.multinomial(1, predications, 1)
+
+
+def train(model, training_data, training_data_labels, 
+          validation_data, validation_data_labels):
+    print("Training model...")
+    for iteration in range(1, 60):
+        print("Iteration", iteration, "...")
+        model.fit(training_data, training_data_labels, batch_size=128, epochs=1)
+        training_loss = model.evaluate(
+            training_data, training_data_labels, batch_size=128)
+        print("Training loss:", training_loss)
+    print("Training stopped.\n")
+    validation_loss = model.evaluate(
+        validation_data, validation_data_labels, batch_size=128)
+    print("Validation loss:", validation_loss)
+
