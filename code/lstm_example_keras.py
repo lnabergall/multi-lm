9b7b0bd936f9202ca68da0ed0e2cfbc79951504c
Lukas Nabergall <lnabergall@mail.usf.edu>
2017-05-12 03:56:41 -0400

    Refactored

--- code/lstm_example_keras.py
+++ code/lstm_example_keras.py
@@ -14,10 +14,11 @@ import data_processing as dp
 
 HIDDEN_DIM = 128
 LEARNING_RATE = 0.01
+BACKPROP_TIMESTEPS = 50
+BATCH_SIZE = 128
 
 
 def build_model(training_data, training_data_labels):
-    print("Building model...")
     model = Sequential()
     model.add(LSTM(HIDDEN_DIM, input_shape=(
         training_data.shape[-2], training_data.shape[-1]), return_sequences=False))
@@ -40,10 +41,9 @@ def sample(probability_array, temperature=1.0):
 
 def train(model, training_data, training_data_labels, 
           validation_data, validation_data_labels):
-    print("Training model...")
     for iteration in range(1, 60):
         print("Iteration", iteration, "...")
-        model.fit(training_data, training_data_labels, batch_size=128, epochs=1)
+        model.fit(training_data, training_data_labels, batch_size=BATCH_SIZE, epochs=1)
         training_loss = model.evaluate(
             training_data, training_data_labels, batch_size=1)
         print("Training loss:", training_loss)
@@ -57,10 +57,12 @@ def main():
     print("Fetching data...")
     data = dp.get_data()
     print("Processing data...")
-    processed_data = dp.process_data(data)
+    processed_data = dp.process_data(data, BACKPROP_TIMESTEPS)
     print("Vectorizing data...")
-    vectorized_data = dp.vectorize_data(processed_data)
+    vectorized_data = dp.vectorize_data(processed_data, BACKPROP_TIMESTEPS)
+    print("Building model...")
     model = build_model(vectorized_data[0], vectorized_data[1])
+    print("Training model...")
     train(model, vectorized_data[0], vectorized_data[1], 
           vectorized_data[2], vectorized_data[3])
 
