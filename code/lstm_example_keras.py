1f7adf6dd504092cd4ef4ca68652c03bdf872c79
Lukas Nabergall <lnabergall@mail.usf.edu>
2017-05-13 04:27:18 -0400

    Added training and validation loss calculation and generating example code for a given description

--- code/lstm_example_keras.py
+++ code/lstm_example_keras.py
@@ -2,6 +2,7 @@
 Example implementation of a vanilla LSTM network in Keras.
 """
 
+from statistics import mean
 from random import shuffle
 from collections import OrderedDict
 
@@ -15,7 +16,7 @@ import data_processing as dp
 
 
 HIDDEN_DIM = 128
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.005
 BACKPROP_TIMESTEPS = 50
 BATCH_SIZE = 128
 SUPER_BATCHES = 50
@@ -52,6 +53,39 @@ def sample(probability_array, temperature=1.0):
     return np.random.multinomial(1, predications, 1)
 
 
+def evaluate_model(model, data_dict, description_chars, script_chars):
+    data, data_labels = [], []
+    for description, solution_dict in data_dict.items():
+        script_arrays = []
+        for i, script in enumerate(solution_dict["python"]):
+            description_array, script_array = dp.vectorize_example(
+                description, script, description_chars, script_chars, 
+                as_collection=True)
+            script_arrays.append(script_array)
+            if i == 0:
+                data.append(description_array)
+        data_labels.append(script_arrays)
+
+    loss, count = 0, 0
+    for desc_array, script_arrays in zip(data, data_labels):
+        for script_array in script_arrays:
+            loss += model.evaluate(desc_array, script_array)
+            count += 1
+
+    return loss / count
+
+
+def generate_output(model, description_array, diversity, script_chars):
+    script_indices_char = {i: char for i, char in enumerate(script_chars)}
+    predictions = model.predict(description_array)
+    output = ""
+    for i in range(predications.shape[0]):
+        next_index = sample(predications[i], diversity)
+        next_char = script_indices_char[next_index]
+        output += next_char
+    return output
+
+
 def main():
     print("\nFetching data...")
     data = dp.get_data()
@@ -62,33 +96,51 @@ def main():
     training_data_dict = OrderedDict(processed_data["training_data"])
     validation_data_dict = processed_data["validation_data"]
     test_data_dict = processed_data["test_data"]
-    # training_data, training_data_labels = dp.vectorize_data(
-    #     training_data_dict, description_chars, script_chars)
+
     print("\nBuilding model...")
     model = build_model(len(description_chars), len(script_chars))
-    for iteration in range(1, 60):
-        training_data_dict = shuffle_dict(training_data_dict)
-        for index in range(SUPER_BATCHES):
-            print("\nVectorizing data...")
-            description_count = len(training_data_dict.keys())
-            super_batch_dict = {desc: training_data_dict[desc] 
-                for i, desc in enumerate(training_data_dict) 
-                if i in range((index*description_count)//SUPER_BATCHES, 
-                              ((index+1)*description_count)//SUPER_BATCHES + 1)}
-            super_batch, super_batch_labels = dp.vectorize_data(
-                super_batch_dict, description_chars, script_chars, BACKPROP_TIMESTEPS)
-            print("Training model...")
-            model.fit(super_batch, super_batch_labels, batch_size=BATCH_SIZE, epochs=1)
-            # training_loss = model.evaluate(
-            #     training_data, training_data_labels, batch_size=1)
-            # print("Training loss:", training_loss)
+    try:
+        for iteration in range(1, 60):
+            training_data_dict = shuffle_dict(training_data_dict)
+            for index in range(SUPER_BATCHES):
+                print("\n\n-------- Iteration " + str(iteration) 
+                      + ", Super Batch " + str(index) + "...")
+                print("Vectorizing data...")
+                description_count = len(training_data_dict.keys())
+                super_batch_dict = {desc: training_data_dict[desc] 
+                    for i, desc in enumerate(training_data_dict) 
+                    if i in range((index*description_count)//SUPER_BATCHES, 
+                                  ((index+1)*description_count)//SUPER_BATCHES + 1)}
+                super_batch, super_batch_labels = dp.vectorize_data(
+                    super_batch_dict, description_chars, 
+                    script_chars, BACKPROP_TIMESTEPS)
+                print("Training model...")
+                model.fit(super_batch, super_batch_labels, 
+                          batch_size=BATCH_SIZE, epochs=1)
+                for diversity in [0.2, 0.4, 0.6, 1.0]:
+                    print("\n---- diversity:", diversity)
+                    description = list(validation_data_dict.keys())[-1]
+                    script = validation_data_dict[description]["python"][0]
+                    print("-- Description:")
+                    print(description)
+                    description_array, _ = dp.vectorize_example(
+                        description, script, description_chars, 
+                        script_chars, as_collection=True)
+                    generated_script = generate_output(
+                        model, description_array, diversity, script_chars)
+                    print("-- Generated Script:")
+                    print(generated_script)
+                print("\nCalculating training loss...")
+                training_loss = evaluate_model(
+                    model, training_data_dict, description_chars, script_chars)
+                print("Training loss:", training_loss)
+    except KeyboardInterrupt:
+        pass
     print("\nTraining stopped.")
-    # print("\nVectorizing data...")
-    # validation_data, validation_data_labels = dp.vectorize_data(
-    #     validation_data_dict, description_chars, script_chars)
-    # validation_loss = model.evaluate(
-    #     validation_data, validation_data_labels, batch_size=1)
-    # print("Validation loss:", validation_loss)
+
+    validation_loss = evaluate_model(
+        model, validation_data_dict, description_chars, script_chars)
+    print("Validation loss:", validation_loss)
 
 
 if __name__ == '__main__':
