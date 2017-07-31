9d74f1631561d556bf371816210c7f8951860c1f
Lukas Nabergall <lnabergall@mail.usf.edu>
2017-07-31 05:41:54 -0400

    Added various LSTM hyperparameter configurations

--- code/tensor2tensor_model.py
+++ code/tensor2tensor_model.py
@@ -7,10 +7,11 @@ Four primary models: LSTM, LSTM + Mixture-of-Experts, Attention Network,
 
 TODO: Add character convolution neural network input layer (modality?)
       Implement LSTM + MoE by adding MoE + residual connections to BasicLSTMCell
+      May need to implement importance sampling (or some other scheme...)
 """
 
 from tensor2tensor.models import (lstm, attention_lm, attention_lm_moe, 
-                                  common_layers)
+                                  common_layers, common_hparams)
 from tensor2tensor.utils import registry, t2t_model
 
 
@@ -27,5 +28,68 @@ class LSTMLm(t2t_model.T2TModel):
                                    self._hparams, train, "lstm")
             return tf.expand_dims(outputs, axis=2)
 
+
+@registry.register_hparams
+def lstm_literature_base():
+    """Set of base hyperparameters for LSTM from Jozefowicz et al."""
+    hparams = common_hparams.basic_params1()
+    hparams.clip_grad_norm = 1.0
+    hparams.label_smoothing = 0.0
+    hparams.batch_size = 1024
+    hparams.optimizer = "Adagrad"
+    hparams.learning_rate = 0.2
+    return hparams
+
+
+@registry.register_hparams
+def lstm_literature_small():
+    """Set of hyperparameters for small LSTM from Jozefowicz et al."""
+    hparams = lstm_literature_base()
+    hparams.hidden_size = 512
+    hparams.num_hidden_layers = 1
+    hparams.dropout = 0.1
+
+
+@registry.register_hparams
+def lstm_literature_large():
+    """Set of hyperparameters for largest LSTM from Jozefowicz et al."""
+    hparams = lstm_literature_base()
+    hparams.hidden_size = 8192
+    hparams.num_hidden_layers = 2
+    hparams.dropout = 0.25
+
+
+@registry.register_hparams
+def lstm_base():
+    """Set of hyperparameters for our LSTM."""
+    hparams = common_hparams.basic_params1()
+    hparams.batch_size = 1024
+    hparams.label_smoothing = 0.0
+    hparams.shared_embedding_and_softmax_weights = int(True)
+    return hparams
+
+
+@registry.register_hparams
+def lstm_small():
+    """Set of hyperparameters for our LSTM."""
+    hparams = lstm_base()
+    hparams.hidden_size = 256
+    hparams.num_hidden_layers = 1
+
+
+@registry.register_hparams
+def lstm_medium():
+    """Set of hyperparameters for our LSTM."""
+    hparams = lstm_base()
+    hparams.hidden_size = 1024
+    params.num_hidden_layers = 2
+
+
+@registry.register_hparams
+def lstm_large():
+    """Set of hyperparameters for our LSTM."""
+    hparams = lstm_base()
+    hparams.hidden_size = 8192
+    params.num_hidden_layers = 4
 
 
